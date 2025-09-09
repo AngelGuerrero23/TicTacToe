498a59d02004ca8ef7efce5f3a9283d85cc7365c
angel <angel_guerrero1@ucne.edu.do>
2025-09-08 21:04:13 -0400

    Carpeta innecesaria

--- TicTacToe/Models/Jugadores.cs
+++ TicTacToe/Models/Jugadores.cs
@@ -8,10 +8,10 @@ namespace TicTacToe.Models
         [Key]
 
         [Required(ErrorMessage = "Este campo es requerido")]
+        
         public int JugadorId { get; set; }
 
         [Required(ErrorMessage = "Este campo es requerido")]
-
         public string JugadorName { get; set; } = null!;
 
         [Range(0, int.MaxValue, ErrorMessage="Las partidas deben ser  0 o mayor")]
